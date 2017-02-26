267b83c0dffb39737672b1b90513c4c21e021580
Tom Bryant <me@tombryant.co.uk>
2017-02-26 20:54:09 +0000

    Initial Commit

---
+++ Mix/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mix
+//
+//  Created by Tom Bryant on 26/02/2017.
+//  Copyright © 2017 Tom Bryant. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
